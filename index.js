31941d5989972a27c8ed5d34bb6621f859b98e3c
Vivian <vivian@qwilr.com>
2019-08-01 14:22:22 +1000

    Hooked up a side by side widget with placeholder sides

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-interact(".image-resizer")
+interact(".resize-editor")
   .resizable({
     edges: {
       top: ".resize-top",
@@ -8,16 +8,43 @@ interact(".image-resizer")
     },
 
     modifiers: [
-      // keep the edges inside the parent
-      interact.modifiers.restrictEdges({
-        outer: "parent",
-        endOnly: true
-      }),
-
-      // minimum size
       interact.modifiers.restrictSize({
         min: { width: 50 },
         max: { width: 800 }
+      })
+    ],
+
+    inertia: true
+  })
+  .on("resizemove", function(event) {
+    var target = event.target;
+    var x = parseFloat(target.getAttribute("data-x")) || 0;
+    var y = parseFloat(target.getAttribute("data-y")) || 0;
+
+    // update the element's style
+    target.style.width = event.rect.width + "px";
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.webkitTransform = target.style.transform =
+      "translate(" + x + "px," + y + "px)";
+  });
+
+interact(".resize-sbs")
+  .resizable({
+    edges: {
+      top: ".resize-top",
+      left: ".resize-left",
+      bottom: ".resize-bottom",
+      right: ".resize-right"
+    },
+
+    modifiers: [
+      interact.modifiers.restrictSize({
+        min: { width: 50 },
+        max: { width: 480 }
       })
     ],
 
@@ -62,14 +89,26 @@ window.editorFns = {
     const key = event.key; // const {key} = event; ES6+
     if (key === "Enter") {
       e.preventDefault();
-      const $newLine = $(
-        `<p
-          contenteditable="true"
-          onkeydown="window.editorFns.onKeyDownEditor(event)"
-          onfocus="window.editorFns.onFocusEditor(event)"
-          onblur="window.editorFns.onBlurEditor(event)"
-        ></p>`
-      );
+      let $newLine;
+      if (e.target.tagName === "H1") {
+        $newLine = $(
+          `<h1
+            contenteditable="true"
+            onkeydown="window.editorFns.onKeyDownEditor(event)"
+            onfocus="window.editorFns.onFocusEditor(event)"
+            onblur="window.editorFns.onBlurEditor(event)"
+          ></h1>`
+        );
+      } else {
+        $newLine = $(
+          `<p
+            contenteditable="true"
+            onkeydown="window.editorFns.onKeyDownEditor(event)"
+            onfocus="window.editorFns.onFocusEditor(event)"
+            onblur="window.editorFns.onBlurEditor(event)"
+          ></p>`
+        );
+      }
       $newLine.insertAfter(e.target);
       $newLine.focus();
       return false;
@@ -79,8 +118,11 @@ window.editorFns = {
         e.target.textContent === "" &&
         !e.target.classList.contains("undeletable")
       ) {
-        e.target.previousSibling.focus();
-        e.target.remove();
+        try {
+          e.target.previousElementSibling.focus();
+        } finally {
+          e.target.remove();
+        }
       }
       return false;
     }
@@ -88,15 +130,26 @@ window.editorFns = {
   },
   onFocusEditor: e => {
     const elWA = document.getElementById("widget-adder");
-    const { top, height } = e.target.getBoundingClientRect();
 
-    elWA.style.top = `${top + height / 2}px`;
+    // hide sbs option if in sbs
+    if (e.target.closest(".sbs") !== null) {
+      elWA.classList.add("hide-sbs");
+    } else {
+      elWA.classList.remove("hide-sbs");
+    }
+
+    // position the widget adder to the focussed line
+    const { top, left, height } = e.target.getBoundingClientRect();
+    elWA.style.top = `${window.scrollY + top + height / 2}px`;
+    elWA.style.left = `${window.scrollX + left - 40}px`;
+
     window.lastFocussedLine = e.target;
   },
   onBlurEditor: e => {
     if (e.target.id !== "widget-adder") {
       const elWA = document.getElementById("widget-adder");
       elWA.style.top = `-100px`;
+      elWA.style.left = `-100px`;
       elWA.classList.remove("show-menu");
     }
   },
@@ -116,11 +169,20 @@ window.editorFns = {
       </div>`
     );
 
+    if (window.lastFocussedLine.closest(".sbs") !== null) {
+      $imageResizer.addClass("resize-sbs");
+    } else {
+      $imageResizer.addClass("resize-editor");
+    }
+
     let imageUrl = prompt("Enter image url: ");
+    if (imageUrl === null) {
+      return;
+    }
     var elImg = new Image();
     elImg.onload = function() {
       // Get image width and set the resizer to that width
-      const width = img.width;
+      const width = elImg.width;
       $imageResizer.style.width = width;
     };
     elImg.src = imageUrl;
@@ -135,10 +197,94 @@ window.editorFns = {
       ></p>`
     );
     $imageResizer.insertAfter(window.lastFocussedLine);
-    $newLine.insertAfter($image);
+    $newLine.insertAfter($imageResizer);
   },
   onClickAddSbs: e => {
     e.preventDefault();
+    const $sbs = $(
+      `<div class="sbs">
+        <div class="sbs-left">
+          <h1
+            contenteditable="true"
+            onkeydown="window.editorFns.onKeyDownEditor(event)"
+            onfocus="window.editorFns.onFocusEditor(event)"
+            onblur="window.editorFns.onBlurEditor(event)"
+          ></h1>
+          <p
+            contenteditable="true"
+            onkeydown="window.editorFns.onKeyDownEditor(event)"
+            onfocus="window.editorFns.onFocusEditor(event)"
+            onblur="window.editorFns.onBlurEditor(event)"
+          ></p>
+        </div>
+        <div class="sbs-right">
+          <div
+            class="image-placeholder"
+            onclick="window.editorFns.onClickImagePlaceholder(event)"
+          >
+            <p><img src="assets/icons/images-monotone.svg" /></p>
+            <h4>
+              <strike>Drag an image in, or</strike> click to browse images
+            </h4>
+
+            <img
+              class="delete-button"
+              onclick="window.editorFns.onClickDeleteImagePlaceholder(event)"
+              src="assets/icons/Delete.svg"
+            />
+          </div>
+          <p
+            contenteditable="true"
+            onkeydown="window.editorFns.onKeyDownEditor(event)"
+            onfocus="window.editorFns.onFocusEditor(event)"
+            onblur="window.editorFns.onBlurEditor(event)"
+          ></p>
+        </div>
+      </div>`
+    );
+
+    const $newLine = $(
+      `<p
+        contenteditable="true"
+        onkeydown="window.editorFns.onKeyDownEditor(event)"
+        onfocus="window.editorFns.onFocusEditor(event)"
+        onblur="window.editorFns.onBlurEditor(event)"
+      ></p>`
+    );
+    $sbs.insertAfter(window.lastFocussedLine);
+    $newLine.insertAfter($sbs);
+    $sbs.find("h1").focus();
+  },
+
+  onClickImagePlaceholder: e => {
+    e.preventDefault();
+    const elImagePlaceholder = e.target.closest(".image-placeholder");
+
+    const $imageResizer = $(
+      `<div class="image-resizer resize-sbs" onclick="window.editorFns.onClickImage(event)">
+        <div class="resize-handle resize-top resize-left"></div>
+        <div class="resize-handle resize-top resize-right"></div>
+        <div class="resize-handle resize-bottom resize-left"></div>
+        <div class="resize-handle resize-bottom resize-right"></div>
+      </div>`
+    );
+
+    let imageUrl = prompt("Enter image url: ");
+    if (imageUrl === null) {
+      return;
+    }
+    var elImg = new Image();
+    elImg.src = imageUrl;
+    $imageResizer.width("480px");
+    $imageResizer.append(elImg);
+    $imageResizer.insertAfter(elImagePlaceholder);
+    elImagePlaceholder.remove();
+  },
+  onClickDeleteImagePlaceholder: e => {
+    e.preventDefault();
+    e.stopPropagation();
+    const elImagePlaceholder = e.target.closest(".image-placeholder");
+    elImagePlaceholder.remove();
   }
 };
 
